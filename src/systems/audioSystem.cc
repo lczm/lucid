58aea27b75b373148e2144dc3aba0b31d6a7189a
ossaris56 <ossaris56@gmail.com>
2020-12-03 00:01:25 +0800

    stopped audio frm autoplaying

--- src/systems/audioSystem.cc
+++ src/systems/audioSystem.cc
@@ -12,28 +12,28 @@ AudioSystem::AudioSystem() {
 AudioSystem::~AudioSystem() = default;
 
 void AudioSystem::Update(double dt, Registry* registry, Input* input) {
-  std::vector<void*> seComponents = registry->GetComponents<SoundEffect, Transform>();
-  std::vector<void*> musicComponents = registry->GetComponents<Music>();
+  //std::vector<void*> seComponents = registry->GetComponents<SoundEffect, Transform>();
+  //std::vector<void*> musicComponents = registry->GetComponents<Music>();
 
-  auto* soundEffects = static_cast<ComponentVector<SoundEffect>*>(seComponents[0]);
-  auto* soundEffectsTransform = static_cast<ComponentVector<Transform>*>(seComponents[1]);
-  auto* music = static_cast<ComponentVector<Music>*>(musicComponents[0]);
+  //auto* soundEffects = static_cast<ComponentVector<SoundEffect>*>(seComponents[0]);
+  //auto* soundEffectsTransform = static_cast<ComponentVector<Transform>*>(seComponents[1]);
+  //auto* music = static_cast<ComponentVector<Music>*>(musicComponents[0]);
 
-  // gruntcooldown just so it doesnt play a million times a second
-  gruntCooldown += dt;
-  if (gruntCooldown > 1) {
-    // Play sound effects
-    for (size_t i = 0; i < soundEffects->Size(); i++) {
-      SoundEffect& se = soundEffects->At(i);
-      Transform& seTransform = soundEffectsTransform->At(i);
-      int sound = SoundEffectsLibrary::Get()->Load(se.filePath);
-      AudioVariables::sePlayer.SetLooping(se.looping);
-      AudioVariables::sePlayer.SetPosition(seTransform.position.x, seTransform.position.y, seTransform.position.z);
-      AudioVariables::sePlayer.Play(sound);
-      // TODO: remove component once its been implemented;
-      gruntCooldown = 0;
-    }
-  }
+  //// gruntcooldown just so it doesnt play a million times a second
+  //gruntCooldown += dt;
+  //if (gruntCooldown > 1) {
+  //  // Play sound effects
+  //  for (size_t i = 0; i < soundEffects->Size(); i++) {
+  //    SoundEffect& se = soundEffects->At(i);
+  //    Transform& seTransform = soundEffectsTransform->At(i);
+  //    int sound = SoundEffectsLibrary::Get()->Load(se.filePath);
+  //    AudioVariables::sePlayer.SetLooping(se.looping);
+  //    AudioVariables::sePlayer.SetPosition(seTransform.position.x, seTransform.position.y, seTransform.position.z);
+  //    AudioVariables::sePlayer.Play(sound);
+  //    // TODO: remove component once its been implemented;
+  //    gruntCooldown = 0;
+  //  }
+  //}
 
   // Play Music
   // for (size_t i = 0; i < music->Size(); i++) {
