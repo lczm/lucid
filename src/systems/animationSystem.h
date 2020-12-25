9adabbf6b7e83e60800955fa32c42419f0d3298d
lczm <zemingchua@gmail.com>
2020-12-25 22:09:56 +0800

    Add animating interpolation boilerplate

--- src/systems/animationSystem.h
+++ src/systems/animationSystem.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <string>
 #include <iostream>
 
 #include "glm.hpp"
@@ -8,6 +9,7 @@
 #include "ecs.h"
 #include "component.h"
 #include "engineComponents.h"
+#include "mesh.h"
 #include "model.h"
 #include "shader.h"
 #include "shaderResource.h"
@@ -20,6 +22,18 @@ class AnimationSystem : public System
 
   void Update(float dt, Registry* registry, Input* input);
 
-  void SetBoneTransform(float dt, Registry* registry, Model& model);
-  void BoneTransform(float dt, std::vector<glm::mat4> transforms);
+  void SetBoneTransform(float dt, Registry* registry, Mesh& mesh);
+  void BoneTransform(float dt, Mesh& mesh, std::vector<glm::mat4>& transforms);
+  void ReadNodeHierarchy(float animTime, const aiScene* scene, const aiNode* node,
+                         glm::mat4& parentTransform);
+  const aiNodeAnim* FindNodeAnim(const aiAnimation* pAnimation, const std::string nodeName);
+
+  uint32_t FindScaling(float animTime, const aiNodeAnim* pNodeAnim);
+  uint32_t FindRotation(float animTime, const aiNodeAnim* pNodeAnim);
+  uint32_t FindPosition(float animTime, const aiNodeAnim* pNodeAnim);
+
+  void CalculateInterpolatedScaling(aiVector3D& vec, float animTime, const aiNodeAnim* pNodeAnim);
+  void CalculateInterpolatedRotation(aiQuaternion& quat, float animTime,
+                                     const aiNodeAnim* pNodeAnim);
+  void CalculateInterpolatedPosition(aiVector3D& vec, float animTime, const aiNodeAnim* pNodeAnim);
 };
