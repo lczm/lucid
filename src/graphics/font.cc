20f0d3fcba33e4a13e0c9d2293385f22c3f662a0
Jasper Chua <s10177296@connect.np.edu.sg>
2021-02-03 15:12:05 +0800

    added setting font char size

--- src/graphics/font.cc
+++ src/graphics/font.cc
@@ -10,18 +10,26 @@ Font::Font(std::string path)
   }
 
   FT_Error fileError = FT_New_Face(library, path.c_str(), 0, &face);
-  if (error == FT_Err_Unknown_File_Format)
+  if (fileError == FT_Err_Unknown_File_Format)
   {
     std::cout << "Freetype Error: Font file can be read but is not in a supported format"
               << std::endl;
     return;
   }
-  else if (error)
+  else if (fileError)
   {
     std::cout << "Freetype Error: Font file could not be opened or read, or font file is broken"
               << std::endl;
     return;
   }
+
+  FT_Error charSizeError = FT_Set_Char_Size(face, 0, 0, 1440, 900);
+
+  if (charSizeError)
+  {
+    std::cout << "Freetype Error: Could not set character size" std << endl;
+  }
+
   std::cout << "Freetype: Font loaded" << std::endl;
 
   Font::path = path;
