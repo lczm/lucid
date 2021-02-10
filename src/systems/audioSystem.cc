33febb775424ede07bbb26067438c38c9896a24a
ossaris56 <ossaris56@gmail.com>
2021-02-10 14:03:48 +0800

    changed audiosystem to use eachexact

--- src/systems/audioSystem.cc
+++ src/systems/audioSystem.cc
@@ -10,7 +10,12 @@ AudioSystem::~AudioSystem() = default;
 
 void AudioSystem::Update(float dt, Registry* registry, Input* input)
 {
-  registry->GetComponentsIter<Sound, Transform>()->Each([&](Sound& sound, Transform& transform) {
+  Listener listener = registry->GetResource<Listener>();
+  sd->SetLocation(listener.x, listener.y, listener.z);
+  // TODO: add feature to change orientation
+
+  // Sound without transform default plays at where the listener is
+  registry->GetComponentsIter<Sound>()->EachExact([&](Sound& sound) {
     ALuint soundBuffer = seLibrary->Load(sound.filePath);
 
     // if its a new sound that hasnt been played yet
@@ -19,8 +24,7 @@ void AudioSystem::Update(float dt, Registry* registry, Input* input)
       SoundEffectsPlayer* player = new SoundEffectsPlayer();
       bufferMap[soundBuffer] = player;
     }
-    bufferMap[soundBuffer]->SetPosition(transform.position.x, transform.position.y,
-                                        transform.position.z);
+    bufferMap[soundBuffer]->SetPosition(listener.x, listener.y, listener.z);
 
     if (sound.play)
     {
@@ -33,7 +37,28 @@ void AudioSystem::Update(float dt, Registry* registry, Input* input)
     seLibrary->UnLoad(soundBuffer);
   });
 
-  Listener listener = registry->GetResource<Listener>();
-  sd->SetLocation(listener.x, listener.y, listener.z);
-  // TODO: add feature to change orientation
+  // Sound with transform plays at where the transform is
+  registry->GetComponentsIter<Sound, Transform>()->EachExact(
+      [&](Sound& sound, Transform& transform) {
+        ALuint soundBuffer = seLibrary->Load(sound.filePath);
+
+        // if its a new sound that hasnt been played yet
+        if (!bufferMap.count(soundBuffer))
+        {
+          SoundEffectsPlayer* player = new SoundEffectsPlayer();
+          bufferMap[soundBuffer] = player;
+        }
+        bufferMap[soundBuffer]->SetPosition(transform.position.x, transform.position.y,
+                                            transform.position.z);
+
+        if (sound.play)
+        {
+          sd->SetGain(sound.gain);
+          bufferMap[soundBuffer]->SetLooping(sound.looping);
+          bufferMap[soundBuffer]->Play(soundBuffer);
+          sound.play = false;
+        }
+
+        seLibrary->UnLoad(soundBuffer);
+      });
 }
